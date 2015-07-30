fc840ed3ec54d758ccbb793b1b7a328b88289921
cit_nvillareal@yahoo.com <cit_nvillareal@yahoo.com>
2015-07-31 02:54:00 +0800

    optimizing...

--- js/jquery.bootstrap.autocomplete.js
+++ js/jquery.bootstrap.autocomplete.js
@@ -11,13 +11,13 @@
 		templates: {
 			"content-wrapper": "<div class=\"autoComplete-wrapper\"></div>",
 			"list-group": "<div class=\"list-group\"></div>",
-			"list-group-item": "<a class=\"list-group-item\" href=\"#\"><%=common_title%></a>",
+			"list-group-item": "<a class=\"list-group-item\" href=\"#\"><%=description%></a>",
 			"bv-list-group-item": "<a class=\"list-group-item\" href=\"#\"><%=value%></a>"
 		},
 		borderRadius: 5,
 		limit: 5,
 		vent: {
-			onselect: function(e, itemData, _this) {
+			onselect: function ( e, itemData, _this ) {
 				// TO DO
 			}
 		},
@@ -26,63 +26,63 @@
 	};
 
 	var Key = {
-	  _pressed: false,
+		_pressed: false,
 
-	  LEFT: 37,
-	  UP: 38,
-	  RIGHT: 39,
-	  DOWN: 40,
-	  ENTER: 13,
-	  SPACE: 32,
-	  
-	  isDown: function(keyCode) {
-	      return (this._pressed == keyCode);
-	  },
-	  
-	  hasKeyDown: function() {
-	      return (this._pressed !== false); 
-	  },
+		LEFT: 37,
+		UP: 38,
+		RIGHT: 39,
+		DOWN: 40,
+		ENTER: 13,
+		SPACE: 32,
+		  
+		isDown: function ( keyCode ) {
+		    return ( this._pressed == keyCode );
+		},
+		  
+		hasKeyDown: function () {
+		    return (this._pressed !== false); 
+		},
 
-	  onKeydown: function(event) {
-	    this._pressed = event.keyCode;
-	  }
+		onKeydown: function ( event ) {
+		  this._pressed = event.keyCode;
+		}
 	};
 
-	function Plugin(el, opt) {
+	function Plugin ( el, opt ) {
 		var _this = this;
 
 		_this.el = el;
-		_this.$el = $(_this.el);
+		_this.$el = $( _this.el );
 
-		_this.options = $.extend (true, {}, defaults, opt ) ;
+		_this.options = $.extend ( true, {}, defaults, opt ) ;
 
 		_this._defaults = defaults;
         _this._name = pluginName;
 
-        _this.$el.bind("keydown", function(e){
-        	Key.onKeydown(e);
-        });
+        _this.$el.bind( "keydown", function( e ){
+        	Key.onKeydown( e );
+        } );
 
 		_this.init();
 	}
 
-	Plugin.prototype.init = function() {
+	Plugin.prototype.init = function () {
 		var _this = this;
 
 		_this.lastKeyWord = "";
 
-		_this.$el.wrap(_this.template("content-wrapper"));
-		_this.$input = (_this.$el.is("input"))? _this.$el: $(_this.$el.find("input"));
+		_this.$el.wrap( _this.template( "content-wrapper" ) );
+		_this.$input = ( _this.$el.is( "input" ) )? _this.$el: $(_this.$el.find( "input" ));
 		
 		_this.$parentElement = _this.$el.parent();
 
-		_this.$parentElement.find("*").css({ "border": 0, "border-radius": 0 });
+		_this.$parentElement.find( "*" ).css( { "border": 0, "border-radius": 0 } );
 
-		_this.$el.css({
+		_this.$el.css( {
 			"border": "1px solid #ccc",
 			"border-radius": _this.options.borderRadius + "px",
 			"overflow": "hidden"
-		});
+		} );
 
 		var position = _this.$el.position();
 		var width = _this.$el.outerWidth();
@@ -103,129 +103,132 @@
 
 		_this.$parentElement.append(_this.$listGroup);
 
-		_this.$input.bind("keyup", function(e){
+		_this.$input.bind ( "keyup", function( e ) {
 			e.preventDefault();
-			_this.performSearch(e);
-		});
+			_this.onKeyUp( e );
+		} );
 
-		_this.$input.focusout(function(){
+		_this.$input.focusout ( function (){
 			_this.hideListGroup();
-		});
+		} );
 	};
 
-	Plugin.prototype.performSearch = function(e) {
+	Plugin.prototype.onKeyUp = function ( e ) {
 		var _this = this;
+		if( Key.isDown ( Key.ENTER ) ) {
+			_this.hideListGroup();
+		} else if( Key.isDown( Key.DOWN ) || Key.isDown ( Key.UP )) {
+			_this.onSelection ( e );
+		} else if(_this.options.url == false) {
+		   _this.search(_this.$input.val());
+			_this.showHideListGroup();
+		} else {
+			_this.remoteSearch(_this.$input.val());
+			_this.showHideListGroup();
+		}
+	}
 
+	Plugin.prototype.remoteSearch = function(keyword) {
 		if(typeof _this.timeoutId != undefined) {
 			clearTimeout(_this.timeoutId);
 		}
 
-		if(Key.isDown(Key.ENTER)) {
-			_this.hideListGroup();
-			_this.$listGroup.html("");
-		} else if(Key.isDown(Key.DOWN) || Key.isDown(Key.UP)) {
-			_this.onSelection(e);
-			_this.showHideListGroup();
-		} else {
-			if(_this.options.url == false) {
-			   _this.search(_this.$input.val());
-			   _this.showHideListGroup();
-			} else {
-				_this.timeoutId = setTimeout(function(){
-					var value = _this.$input.val();
-					if(value.trim().length > 0) {
-						$.get(_this.options.url + value, function(res) {
-							if(res.data == null) {
-								_this.options.data = [];
-								return;
-							}
+		_this.timeoutId = setTimeout(function(){
 
-							_this.options.data = res.data;
-							if(_this.options["data"].length > 0) {
-								_this.search(_this.$input.val());
-						   		_this.showHideListGroup();
-							}
-
-							clearTimeout(_this.timeoutId);
-						});
-					}
-				}, 100);
+			if(keyword.trim().length <= 0) {
+				return;
 			}
-		}
+
+			$.get(_this.options.url + keyword, function(res) {
+				if(res.data == null) {
+					_this.options.data = [];
+					return;
+				}
+
+				_this.options.data = res.data;
+				if(_this.options[ "data" ].length > 0) {
+					_this.search( _this.$input.val() );
+				}
+
+				clearTimeout(_this.timeoutId);
+			});
+
+		}, 100);
 	}
 
-	Plugin.prototype.search = function(keyword) {
+	Plugin.prototype.search = function ( keyword ) {
 		var _this = this;
-
-		keyword = keyword.trim();
 		var data = _this.options.data;
 		var searchable = _this.options.searchable;
 
 		_this.$listGroup.html("");
 		
-		if(keyword.trim().length > 0 && data.length > 0) {
-			_this.lastKeyWord = keyword;
-			for( var i = 0; i < data.length; i++ ) {
+		if(keyword.trim().length <= 0 && data.length <= 0) {
+			return;
+		}
 
-				if( _this.$listGroup.children().length >= _this.options.limit && _this.options.limit !== false ) { 
-					return;
+		_this.lastKeyWord = keyword;
+		for( var i = 0; i < data.length; i++ ) {
+
+			if( _this.$listGroup.children().length >= _this.options.limit && _this.options.limit !== false ) { 
+				return;
+			}
+
+			var span = $( "<span></span>" );
+				span.addClass( "auto-wrap" );
+				span.css({
+					"font-weight": "bold"
+				});
+
+			if(typeof data [ i ] == "string") {
+				var obj = data [ i ];
+				span.html(_this.lastKeyWord);
+				if(typeof obj != undefined && obj.indexOf(keyword) >= 0) {
+					obj = obj.replace(_this.lastKeyWord, span.clone().wrap('<div>').parent().html());
+					_this.append(obj);
 				}
+			} else {
+				var obj = $.extend( true, {}, data[ i ] );
+				for(var j = 0; j < searchable.length; j++) {
+					var key = searchable[ j ];
+					if(typeof obj[ key ] != undefined && obj[ key ].indexOf( keyword ) >= 0) {
+						span.html( _this.lastKeyWord );
 
-				var span = $("<span></span>");
-					span.addClass("auto-wrap");
-					span.css({
-						"font-weight": "bold"
-					});
-
-				if(typeof data[i] == "string") {
-					var obj = data[i];
-					span.html(_this.lastKeyWord);
-					if(typeof obj != undefined && obj.indexOf(keyword) >= 0) {
-						obj = obj.replace(_this.lastKeyWord, span.clone().wrap('<div>').parent().html());
+						obj [ key ] = obj [ key ].replace( _this.lastKeyWord, span.clone().wrap( '<div>' ).parent().html() );
 						_this.append(obj);
-					}
-				} else {
-					var obj = $.extend(true, {}, data[i]);
-					for(var j = 0; j < searchable.length; j++) {
-						var key = searchable[j];
-						if(typeof obj[key] != undefined && obj[key].indexOf(keyword) >= 0) {
-							span.html(_this.lastKeyWord);
 
-							obj[key] = obj[key].replace(_this.lastKeyWord, span.clone().wrap('<div>').parent().html());
-							_this.append(obj);
-
-							break;
-						}
+						break;
 					}
 				}
 			}
-
 		}
 	}; 
 
-	Plugin.prototype.append = function(obj) {
-		var _this = this;
+	Plugin.prototype.append = function ( obj ) {
+		var _this = this,
+			templateName = ( typeof obj == "string" )? "bv-list-group-item": "list-group-item",
+			objValue = ( typeof obj == "string" )? { "value" : obj }: obj,
+			$el = _this.template(templateName, objValue).css( { "border": 0 } );
 
-		var templateName = (typeof obj == "string")? "bv-list-group-item": "list-group-item";
-		var objValue = (typeof obj == "string")? {"value": obj}: obj;
-
-		var $el = _this.template(templateName, objValue).css({"border": 0});
 		_this.$listGroup.append($el);
 
-		$el.bind("mousedown", function(e){
-			e.preventDefault();
-
-			_this.onSelect(e, obj);
+		$el.bind( "mousedown", function ( e ) {
+			_this.onSelect( e, obj );
 			_this.hideListGroup();
-		});
+		} );
+
+		$el.bind( "select", function( e ) {
+			_this.onSelect(e, obj);
+		} );
 	};
 
-	Plugin.prototype.showListGroup = function() {
+	Plugin.prototype.showListGroup = function () {
 		var _this = this;
 
 		_this.$el.css({
 			"border-radius": _this.options.borderRadius + "px " + _this.options.borderRadius + "px 0px 0px"
 		});
+
 		_this.$listGroup.show();
 
 		$("form").each(function(){
@@ -233,59 +236,64 @@
 		});
 	};
 
-	Plugin.prototype.hideListGroup = function() {
+	Plugin.prototype.hideListGroup = function () {
 		var _this = this;
 
 		_this.$el.css({
 			"border-radius": _this.options.borderRadius + "px"
 		});
+
 		_this.$listGroup.hide();
 
 		$("form").each(function(){
 			$(this).removeAttr("onsubmit");
 		});
+
+		_this.$listGroup.html("");
 	};
 
-	Plugin.prototype.showHideListGroup = function() {
+	Plugin.prototype.showHideListGroup = function () {
 		var _this = this;
 
-		if(_this.$listGroup.text().trim().length > 0) {
+		if( _this.$listGroup.text().trim().length > 0 ) {
 			_this.showListGroup();
 		} else {
 			_this.hideListGroup();
 		}
 	};
 
-	Plugin.prototype.onSelect= function(e, obj) {
+	Plugin.prototype.onSelect= function ( e, obj ) {
+		e.preventDefault();
+
 		var _this = this;
 		var searchable = _this.options.searchable;
 		var value = "";
 
 		if(typeof obj == "string") {
-			var span = $("<div></div>").html(obj).find('.auto-wrap')[0];
+			var span = $( "<div></div>" ).html( obj ).find( '.auto-wrap' )[ 0 ];
 			var content = $(span).html();
 
-			obj = obj.replace($(span).clone().wrap('<div>').parent().html(), content);
+			obj = obj.replace( $( span ).clone().wrap( '<div>' ).parent().html(), content );
 			value = obj;
 		} else {
 			for(var i = 0; i < searchable.length; i++) {
-				var span = $("<div></div>").html(obj[searchable[i]]).find('.auto-wrap')[0];
-				var content = $(span).html();
+				var span = $( "<div></div>" ).html( obj [ searchable [ i ] ] ).find( '.auto-wrap' )[ 0 ];
+				var content = $( span ).html();
 
-				obj[searchable[i]] = obj[searchable[i]].replace($(span).clone().wrap('<div>').parent().html(), content);
+				obj [ searchable [ i ] ] = obj[ searchable [ i ] ].replace( $( span ).clone().wrap( '<div>' ).parent().html(), content );
 			}
 
-			value = obj[_this.options.display];
+			value = obj [ _this.options.display ];
 		}
 
-		_this.$input.val(value);
+		_this.$input.val( value );
 		_this.$input.focus();
-		_this.$input.trigger("change");
+		_this.$input.trigger( "change" );
 
-		_this.options.vent.onselect(e, obj, _this);
+		_this.options.vent.onselect( e, obj, _this );
 	}
 
-	Plugin.prototype.onSelection = function(e, obj) {
+	Plugin.prototype.onSelection = function( e, obj ) {
 		var _this = this;
 
 		var listGroupItems = _this.$listGroup.children();
@@ -293,11 +301,11 @@
 
 		if( currentElement == null ) {
 			if( Key.isDown( Key.DOWN ) ) {
-				$( listGroupItems[ 0 ] ).addClass( _this.options.selectionClassName );
-				$( listGroupItems[ 0 ] ).trigger("mousedown");
-			} else if( Key.isDown( Key.UP ) ) {
-				$( listGroupItems[ listGroupItems.length - 1 ] ).addClass( _this.options.selectionClassName );
-				$( listGroupItems[ listGroupItems.length - 1 ] ).trigger("mousedown");
+				$( listGroupItems [ 0 ] ).addClass( _this.options.selectionClassName );
+				$( listGroupItems [ 0 ] ).trigger( "select" );
+			} else if( Key.isDown ( Key.UP ) ) {
+				$( listGroupItems [ listGroupItems.length - 1 ] ).addClass( _this.options.selectionClassName );
+				$( listGroupItems [ listGroupItems.length - 1 ] ).trigger( "select" );
 			}
 			return;
 		} 
@@ -313,38 +321,38 @@
 				_this.$input.val( _this.lastKeyWord );
 			} else {
 				$( next ).addClass( _this.options.selectionClassName );
-				$( next ).trigger("mousedown");
+				$( next ).trigger( "select" );
 			}
 		} else if( Key.isDown( Key.UP ) ) {
 			if( ( currentIndex - 1 ) < 0 && currentElement != null) {
 				_this.$input.val( _this.lastKeyWord );
 			} else {
 				$( previous ).addClass( _this.options.selectionClassName );
-				$( previous ).trigger("mousedown");
+				$( previous ).trigger( "select" );
 			}
 		}
 	};
 
-	Plugin.prototype.template = function(templateName, obj) {	
+	Plugin.prototype.template = function ( templateName, obj ) {	
 		var _this = this;
 
-		var tmpl = _this.options.templates[templateName];
+		var tmpl = _this.options.templates [ templateName ];
 		
 		if(typeof obj != undefined) {
-			$.each(obj, function(key, value) {
-				tmpl = tmpl.replace("<%="+key+"%>", value);
-			});
+			$.each ( obj, function( key, value ) {
+				tmpl = tmpl.replace( "<%="+key+"%>", value );
+			} );
 		}
 
-		return $(tmpl);
+		return $( tmpl );
 	}
 
 	
 	$.fn[pluginName] = function ( options ) {
-		return this.each(function () {
-		    if (!$.data(this, 'plugin_' + pluginName)) {
-		        $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+		return this.each ( function () {
+		    if ( !$.data ( this, 'plugin_' + pluginName ) ) {
+		        $.data ( this, 'plugin_' + pluginName, new Plugin( this, options ));
 		    }
-		});
+		} );
 	}
-})(jQuery, window, document, "undefined");
+} )( jQuery, window, document, "undefined" );
